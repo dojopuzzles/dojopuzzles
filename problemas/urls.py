14df576dd95bea5674abb549e59ca49d2369e64a
Renne Rocha <rennerocha@gmail.com>
2010-12-15 20:17:42 -0200

    Exibição de listagem de todos os problemas cadastrados.

--- problemas/urls.py
+++ problemas/urls.py
@@ -1,10 +1,14 @@
 #!-*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('dojo.problemas.views',
-    url(r'^$', 'problema_aleatorio', name='problema-aleatorio'),
-    url(r'^(?P<problema_id>\d+)/$', 'exibe_problema', name='exibe-problema'), 
-    url(r'^todos_visualizados/$', 'sem_problemas_novos', name='sem-problemas-novos'),
-    url(r'^nenhum_problema/$', 'sem_problemas', name='nenhum-problema-cadastrado'),
-    url(r'^todos/$', 'todos_problemas', name='todos-problemas'),
+from dojo.problemas.models import Problema
+
+urlpatterns = patterns('',
+    url(r'^$', 'dojo.problemas.views.problema_aleatorio', name='problema-aleatorio'),
+    url(r'^(?P<problema_id>\d+)/$', 'dojo.problemas.views.exibe_problema', name='exibe-problema'), 
+    url(r'^todos_visualizados/$', 'dojo.problemas.views.sem_problemas_novos', name='sem-problemas-novos'),
+    url(r'^nenhum_problema/$', 'dojo.problemas.views.sem_problemas', name='nenhum-problema-cadastrado'),
+    url(r'^todos/$', 'django.views.generic.list_detail.object_list',
+        {'queryset':Problema.objects.all().order_by('titulo'),
+         'paginate_by': 15},  name='todos-problemas'),
 )
