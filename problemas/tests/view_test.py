82f9917d987a598868dbe4b58be6ff3f27eba673
Renne Rocha <rennerocha@gmail.com>
2010-12-28 22:52:53 -0200

    Permitir que o usuário informe quais os problemas que ele não gostou, não o exibindo mais ao solicitar um problema aleatório.

--- problemas/tests/view_test.py
+++ problemas/tests/view_test.py
@@ -9,9 +9,8 @@ from dojo.problemas.models import Problema
 class UrlsTestCase(TestCase):
     """ Testa as URLs da aplicação """
     def setUp(self):
-        titulo = "Título do Problema 1"
-        descricao = "Descrição do Problema 1"
-        self.problema = Problema(titulo=titulo, descricao=descricao)
+        self.problema = Problema(titulo="Título do Problema 1",
+                                 descricao="Descrição do Problema 1")
         self.problema.save()
         self.client = Client()
             
@@ -31,7 +30,53 @@ class UrlsTestCase(TestCase):
         response = self.client.get(reverse('todos-problemas'))
         self.assertNotEqual(response.status_code, 404)
 
+class ExibicaoProblemaTestCase(TestCase):
+    def setUp(self):
+        # Cadastra 2 problemas que serão utilizados nos testes
+        for i in xrange(1,3):
+            titulo = "Título do Problema {0}".format(i)
+            descricao = "Descrição do Problema {0}".format(i)
+            problema = Problema(titulo=titulo, descricao=descricao)
+            problema.save()
+        self.client = Client()
+        
+    def tearDown(self):
+        Problema.objects.all().delete()
+
+    def test_visualizacao_de_problema(self):
+        """ Se o usuário visualizar um problema, ao exibir outro problema, 
+        o problema anterior deve estar na lista de problemas visualizados """
+        problema_exibido1 = Problema.objects.all()[0]
+        response = self.client.get(reverse('exibe-problema', args=[problema_exibido1.id]), follow=True)
+        try:
+            problemas_visualizados = self.client.session['problemas_visualizados']
+            self.assertTrue(problema_exibido1 in problemas_visualizados)
+        except KeyError:
+            self.fail('Não existe lista de problemas visualizados.')
+            
+        problema_exibido2 = Problema.objects.all()[1]
+        response = self.client.get(reverse('exibe-problema', args=[problema_exibido2.id]), follow=True)
+        try:
+            problemas_visualizados = self.client.session['problemas_visualizados']
+            self.assertTrue(problema_exibido1 in problemas_visualizados)
+            self.assertTrue(problema_exibido2 in problemas_visualizados)
+        except KeyError:
+            self.fail('Não existe lista de problemas visualizados.')
+
+    def test_nao_repete_problema_visualizado(self):
+        """ Não é permitido um problema estar na lista de problemas visualizados mais de uma vez """
+        problema_exibido1 = Problema.objects.all()[0]
+
+        response = self.client.get(reverse('exibe-problema', args=[problema_exibido1.id]), follow=True)
+        problemas_visualizados = self.client.session['problemas_visualizados']
+        self.assertEqual(problemas_visualizados.count(problema_exibido1), 1)
+
+        response = self.client.get(reverse('exibe-problema', args=[problema_exibido1.id]), follow=True)
+        problemas_visualizados = self.client.session['problemas_visualizados']
+        self.assertEqual(problemas_visualizados.count(problema_exibido1), 1)
+
 class ProblemaAleatorioTest(TestCase):
+
     def setUp(self):
         # Cadastra 10 problemas que serão utilizados nos testes
         for i in xrange(1,11):
@@ -40,10 +85,13 @@ class ProblemaAleatorioTest(TestCase):
             problema = Problema(titulo=titulo, descricao=descricao)
             problema.save()
         self.client = Client()
-            
+
+    def tearDown(self):
+        Problema.objects.all().delete()
+
     def test_nenhum_problema_cadastrado(self):
-        """ Se nenhum problema estiver cadastrado exibe o template correspondente """
-        Problema.objects.all().delete() # Apaga todos os problemas da base de dados
+        """ Se nenhum problema estiver cadastrado exibe página informando """
+        Problema.objects.all().delete() # Apaga todos os problemas
         response = self.client.get(reverse('problema-aleatorio'))
         self.assertRedirects(response, reverse('nenhum-problema-cadastrado'))
 
@@ -51,24 +99,59 @@ class ProblemaAleatorioTest(TestCase):
         """ Verificando se ao solicitar um problema aleatório o retorno é um problema
         ainda não visualizado ou a informação de que todos os problemas disponíveis
         já foram visualizados """
-        problemas_visualizados = []
         numero_problemas = Problema.objects.count()
         for i in xrange(1, numero_problemas + 1):
             # Como temos 'numero_problemas' problemas de teste cadastrados, 
             # após 'numero_problemas' chamadas de problemas aleatórios, 
             # todos os problemas devem ser visualizados uma única vez
             response = self.client.get(reverse('problema-aleatorio'), follow=True)
-            if 'problema' in response.context:
-                problema_visualizado = response.context['problema']
-
-                if problema_visualizado.id in problemas_visualizados:
-                    self.fail('Reexibindo problema já visualizado')
-
-                problemas_visualizados.append(problema_visualizado.id)
 
         # Após exibir todos os problemas, uma solicitação de um problema 
         # aleatório deve informar que não existe mais nenhum problema novo 
         # cadastrado
-        response = self.client.get(reverse('problema-aleatorio'))
+        response = self.client.get(reverse('problema-aleatorio'), follow=True)
+        self.assertEqual(len(self.client.session['problemas_visualizados']), numero_problemas)
         self.assertRedirects(response, reverse('sem-problemas-novos'))
 
+class ProblemaNaoDesejadoTest(TestCase):
+
+    def setUp(self):
+        # Cadastra 5 problemas que serão utilizados nos testes
+        for i in xrange(1,6):
+            titulo = "Título do Problema {0}".format(i)
+            descricao = "Descrição do Problema {0}".format(i)
+            problema = Problema(titulo=titulo, descricao=descricao)
+            problema.save()
+        self.client = Client()
+
+    def test_se_usuario_nao_gostar_do_problema(self):
+        """ Se o usuário não gostar de um problema, ele não deve mais ser 
+        exibido nos problemas aleatórios e nem na lista de problemas 
+        visualizados """
+        numero_problemas = Problema.objects.count()
+        problema_nao_gostei = Problema.objects.all()[0]
+        response = self.client.get(reverse('exibe-problema', args=[problema_nao_gostei.id]), follow=True)
+        problemas_visualizados = self.client.session['problemas_visualizados']
+
+        try:
+            problemas_nao_gostei = self.client.session['problemas_nao_gostei']
+            self.fail('A lista de problemas que eu não gostei não deve existir ainda.')
+        except KeyError:
+            pass
+
+        self.assertTrue(problema_nao_gostei in problemas_visualizados)
+
+        url_nao_gostei = "{0}?nao_gostei={1}".format(reverse('problema-aleatorio'), problema_nao_gostei.id)
+        response = self.client.get(url_nao_gostei, follow=True)
+
+        try:
+            problemas_visualizados = self.client.session['problemas_visualizados']
+            self.assertFalse(problema_nao_gostei in problemas_visualizados)
+        except KeyError:
+            self.fail('Problema que o usuário não gostou exibido na lista de problemas visualizados.')
+
+        try:
+            problemas_que_nao_gostei = self.client.session['problemas_que_nao_gostei']
+            self.assertTrue(problema_nao_gostei in problemas_que_nao_gostei)
+        except KeyError:
+            self.fail('Problema que o usuário não gostou não está na lista de problemas recusados.')
--- problemas/views.py
+++ problemas/views.py
@@ -17,50 +17,65 @@ def problema_aleatorio(request):
         return HttpResponseRedirect(reverse('nenhum-problema-cadastrado'))
 
     problemas_visualizados = []
-    if "problemas_visualizados" in request.COOKIES:
-        problemas_visualizados = request.COOKIES.get('problemas_visualizados', [])
-        if problemas_visualizados != []:
-            problemas_visualizados = simplejson.loads(problemas_visualizados)
+    if request.session.has_key('problemas_visualizados'):
+        problemas_visualizados = request.session['problemas_visualizados']
 
-    if len(problemas_visualizados) == numero_problemas:
-        return HttpResponseRedirect(reverse('sem-problemas-novos'))
+#    if len(problemas_visualizados) == numero_problemas:
+#        return HttpResponseRedirect(reverse('sem-problemas-novos'))
 
-    # Este modo de obter um registro aleatório é considerado muito
+    problemas_nao_gostei = []
+    if request.session.has_key('problemas_que_nao_gostei'):
+        problemas_nao_gostei = request.session['problemas_que_nao_gostei']
+    if 'nao_gostei' in request.GET:
+        problema_nao_gostei = Problema.objects.get(pk=int(request.GET['nao_gostei']))
+        problemas_visualizados.remove(problema_nao_gostei)
+        request.session['problemas_visualizados'] = problemas_visualizados
+        problemas_nao_gostei.append(problema_nao_gostei)
+        request.session['problemas_que_nao_gostei'] = problemas_nao_gostei
+
+    # TODO Acho que dá para melhorar esse trecho
+    ids_problemas_visualizados = []
+    for problema in problemas_visualizados:
+        ids_problemas_visualizados.append(problema.id)
+
+    ids_problemas_nao_gostei = []
+    for problema in problemas_nao_gostei:
+        ids_problemas_nao_gostei.append(problema.id)
+
+    # TODO Este modo de obter um registro aleatório é considerado muito
     # lento pela documentação do Django. É necessário fazer uma
     # solução melhor.
-    problema_escolhido = Problema.objects.order_by('?')[0]
-    if problema_escolhido.id in problemas_visualizados:
-        while problema_escolhido.id in problemas_visualizados:
-            problema_escolhido = Problema.objects.order_by('?')[0]
+    problemas = Problema.objects.exclude(id__in=ids_problemas_visualizados).exclude(id__in=ids_problemas_nao_gostei).order_by('?')
+    if(len(problemas) == 0):
+        return HttpResponseRedirect(reverse('sem-problemas-novos'))
+    else:
+        problema_escolhido = problemas[0]
+    
 
-    return HttpResponseRedirect(reverse('exibe-problema', args=[problema_escolhido.id]))        
+    return HttpResponseRedirect(reverse('exibe-problema', args=[problema_escolhido.id]))
 
 def exibe_problema(request, problema_id):
     """ Exibe o problema informado """
     try:
         problema = Problema.objects.get(pk = problema_id)
-
-        if "problemas_visualizados" in request.COOKIES:
-            problemas_visualizados = request.COOKIES.get('problemas_visualizados', [])
-            if problemas_visualizados != []:
-                problemas_visualizados = simplejson.loads(problemas_visualizados)
-                if problema.id not in problemas_visualizados:
-                    problemas_visualizados.append(problema.id)
-        else:
-            problemas_visualizados = [problema.id]
-
+        problemas_visualizados = []
+        if request.session.has_key('problemas_visualizados'):
+            problemas_visualizados = request.session['problemas_visualizados']
+        problemas_visualizados.append(problema)
+        problemas_visualizados = list(set(problemas_visualizados))
+        request.session['problemas_visualizados'] = problemas_visualizados
         response = render_to_response('problema.html', locals(), RequestContext(request))
-        response.set_cookie("problemas_visualizados", problemas_visualizados)
-
     except Problema.DoesNotExist:
         raise Http404
-
     return response
 
 def sem_problemas_novos(request):
     """ Exibido se todos os problemas cadastrados já tiverem sido exibidos """
+    problemas_visualizados = []
+    if request.session.has_key('problemas_visualizados'):
+        problemas_visualizados = request.session['problemas_visualizados']
     return render_to_response('sem_problemas_novos.html', locals(), RequestContext(request))
-    
+
 def sem_problemas(request):
     """ Exibido se nenhum problema estiver cadastrado no sistema """
     return render_to_response('sem_problemas.html', locals(), RequestContext(request))
