8459942caa9820f77b3072264169c487a964daf2
Renne Rocha <rennerocha@gmail.com>
2011-01-27 13:59:11 -0200

    Adicionando model ProblemaUtilizado na interface do Admin.

--- problemas/admin.py
+++ problemas/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from dojopuzzles.problemas.models import Problema
+from dojopuzzles.problemas.models import Problema, ProblemaUtilizado
 
 admin.site.register(Problema)
+admin.site.register(ProblemaUtilizado)
 
