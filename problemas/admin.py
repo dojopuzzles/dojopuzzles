05e31539ed7f64dc1fcf0cb7825f27a10986a0d5
Renne Rocha <rennerocha@gmail.com>
2011-05-19 21:13:41 -0300

    Inserindo filtro de problemas publicados/não publicados na interface do Admin.

--- problemas/admin.py
+++ problemas/admin.py
@@ -2,8 +2,11 @@
 from django.contrib import admin
 from dojopuzzles.problemas.models import Problema, ProblemaUtilizado
 
+class ProblemaAdmin(admin.ModelAdmin):
+    list_filter = ('publicado',)
+
 class ProblemaUtilizadoAdmin(admin.ModelAdmin):
     list_display = ('problema', 'data_utilizacao')
 
-admin.site.register(Problema)
+admin.site.register(Problema, ProblemaAdmin)
 admin.site.register(ProblemaUtilizado, ProblemaUtilizadoAdmin)
