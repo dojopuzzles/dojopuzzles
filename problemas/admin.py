e23479804c16dca3a11ddf8b2ab5192835fffbd4
Renne Rocha <rennerocha@gmail.com>
2011-05-19 21:10:24 -0300

    Exibindo a data de utilização na listagem dos problemas utilizados na interface do Admin.

--- problemas/admin.py
+++ problemas/admin.py
@@ -2,5 +2,8 @@
 from django.contrib import admin
 from dojopuzzles.problemas.models import Problema, ProblemaUtilizado
 
+class ProblemaUtilizadoAdmin(admin.ModelAdmin):
+    list_display = ('problema', 'data_utilizacao')
+
 admin.site.register(Problema)
-admin.site.register(ProblemaUtilizado)
+admin.site.register(ProblemaUtilizado, ProblemaUtilizadoAdmin)
