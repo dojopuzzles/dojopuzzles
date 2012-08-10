5517b6057a174dd8633dc9a7212785f14928e4c4
Renne Rocha <me@rennerocha.com>
2012-08-09 21:38:52 -0300

    Removendo DojoPuzzles.com - versão Django 1.2

--- dojopuzzles_old/problemas/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-from model_test import *
-from view_test import *
-from templatetag_test import *
-
-__test__ = {
-        "model_test": model_test,
-        "view_test": view_test,
-        "templatetag_test": templatetag_test
-        }
