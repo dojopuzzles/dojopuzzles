52fec72bf71033fc0e023d33e62b54a25ee2c286
Renne Rocha <me@rennerocha.com>
2012-08-09 22:05:46 -0300

    AJustes do merge

--- dojopuzzles/contribuicoes/urls.py
+++ dojopuzzles/contribuicoes/urls.py
@@ -2,6 +2,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^contribua/$', 'dojopuzzles.contribuicoes.views.contribuicao', name='contribua'),
-    url(r'^recebida/$', 'dojopuzzles.contribuicoes.views.contribuicao_recebida', name='contribuicao-recebida'),
+    url(r'^contribua/$', 'contribuicoes.views.contribuicao', name='contribua'),
+    url(r'^recebida/$', 'contribuicoes.views.contribuicao_recebida', name='contribuicao-recebida'),
 )
