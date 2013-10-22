427b60cbc9a64c0f5a5df1ff3ad76471cfa0e80d
Nicolas Tarzia <contato@nicolastarzia.com>
2013-10-22 17:00:03 -0200

    modified:   dojopuzzles/problemas/admin.py 	modified:   dojopuzzles/dojopuzzles/settings.py 	modified:   dojopuzzles/problemas/models.py 	modified:   dojopuzzles/problemas/templates/problema.html  	modified:   dojopuzzles/problemas/views.py

--- dojopuzzles/problemas/admin.py
+++ dojopuzzles/problemas/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from problemas.models import Problema, ProblemaUtilizado
+from problemas.models import Problema, ProblemaUtilizado, AdministracaoSite
 
 
 class ProblemaAdmin(admin.ModelAdmin):
@@ -10,5 +10,9 @@ class ProblemaAdmin(admin.ModelAdmin):
 class ProblemaUtilizadoAdmin(admin.ModelAdmin):
     list_display = ('problema', 'data_utilizacao')
 
+class AdministracaoSiteAdmin(admin.ModelAdmin):
+	list_display = ('site','disqusKey')
+
 admin.site.register(Problema, ProblemaAdmin)
 admin.site.register(ProblemaUtilizado, ProblemaUtilizadoAdmin)
+admin.site.register(AdministracaoSite, AdministracaoSiteAdmin)
