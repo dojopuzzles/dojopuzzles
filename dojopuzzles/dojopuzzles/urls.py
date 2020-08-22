094abcf18908e12f71af14786f18a9b0eae76b2b
Renne Rocha <renne@rennerocha.com>
2020-08-22 11:59:15 -0300

    Improve URL name

--- dojopuzzles/dojopuzzles/urls.py
+++ dojopuzzles/dojopuzzles/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("/", include("problems.urls")),
+    path("problems/", include("problems.urls")),
 ]
