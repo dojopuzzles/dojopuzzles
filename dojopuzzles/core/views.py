03e769d2dff5c0da44835668e1229258c348afcf
Renne Rocha <renne@rennerocha.com>
2020-08-24 19:56:38 -0300

    Create core application
    This application will store views that are not related to any special
    features (like index and about pages)

---
+++ dojopuzzles/core/views.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class CorePagesViewTestCase(TestCase):
+    def test_access_main_page(self):
+        response = self.client.get(reverse("core:home"))
+        self.assertEqual(response.status_code, 200)
