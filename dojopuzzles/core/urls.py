37c01402509b6d43576524517e226d064ce5a346
berrondo <claudio.berrondo@gmail.com>
2020-09-01 02:53:46 +0000

    fixing / -> core.home

--- dojopuzzles/core/urls.py
+++ dojopuzzles/core/urls.py
@@ -6,6 +6,6 @@ from core import views
 app_name = "core"
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
+    path("", views.home, name="home"),
     path("about/", views.about, name="about"),
 ]
