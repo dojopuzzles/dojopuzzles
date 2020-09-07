f65de42c6e10561a6f9c2bd47dca39ac8f3f7930
Renne Rocha <renne@rennerocha.com>
2020-09-07 12:02:26 -0300

    Add counter of total problems used in Dojos

--- dojopuzzles/core/tests/test_views.py
+++ dojopuzzles/core/tests/test_views.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from model_bakery import baker
+
+from problems.models import Problem
+
 
 class CorePagesViewTestCase(TestCase):
     def test_access_main_page(self):
@@ -10,3 +14,13 @@ class CorePagesViewTestCase(TestCase):
     def test_access_about_page(self):
         response = self.client.get(reverse("core:about"))
         self.assertEqual(response.status_code, 200)
+
+
+class HomeViewTestCase(TestCase):
+    def test_home_has_total_number_of_problems_used_in_context(self):
+        baker.make(Problem, uses=41)
+        baker.make(Problem, uses=1)
+
+        response = self.client.get(reverse("core:home"))
+        self.assertTrue("problems_used" in response.context)
+        self.assertEqual(42, response.context["problems_used"])
