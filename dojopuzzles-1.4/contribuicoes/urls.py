a25423dd801b22ae4a13487e4d0807a3d82635a9
Renne Rocha <renne@rennerocha.com>
2020-09-07 13:01:25 -0300

    Remove old version of application

--- dojopuzzles-1.4/contribuicoes/urls.py
+++
@@ -1,7 +0,0 @@
-#!-*- coding: utf-8 -*-
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    url(r'^contribua/$', 'contribuicoes.views.contribuicao', name='contribua'),
-    url(r'^recebida/$', 'contribuicoes.views.contribuicao_recebida', name='contribuicao-recebida'),
-)
--- dojopuzzles-1.4/problemas/forms.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django import forms
-
-
-class FormBusca(forms.Form):
-    titulo = forms.CharField(max_length=100)
