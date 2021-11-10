708e42853443f42e1c9a467f9edbe8ce0407af3b
Nicolas-R1beiro <94021663+Nicolas-R1beiro@users.noreply.github.com>
2021-11-09 21:32:42 -0300

    Delete start_app.sh

--- start_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-printenv | grep -v "no_proxy" >> /etc/environment
-
-python manage.py collectstatic --noinput
-python manage.py migrate
-
-gunicorn --workers=2 --bind=0.0.0.0:8100 dojopuzzles.wsgi:application
